7e57993f5e43e94dfee87669bfc28afb938fc2f1
Eduardo Fernando Dias <dudu.dias74@gmail.com>
2020-03-01 20:20:37 -0300

    Aula 2 - Atividade 3

---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar( title: Text("Contatos"),
+       ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text("Eduardo", style: TextStyle(fontSize: 24.0),),
+              subtitle: Text("1000", style: TextStyle(fontSize: 16.0),),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
